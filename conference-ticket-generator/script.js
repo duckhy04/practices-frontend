e9ebc5ca274b19cb05e0ae7e1880d635ac663f30
duckhy04 <nguynduchuy12@gmail.com>
2025-01-02 17:18:19 +0700

    Update conference ticket generate

--- conference-ticket-generator/script.js
+++ conference-ticket-generator/script.js
@@ -1,3 +1,67 @@
-document.getElementById('drag-drop-area').addEventListener('click', function () {
-    document.getElementById('uploadAvatar').click();
+const dragDropArea = document.getElementById('drag-drop-area');
+const uploadAvatar = document.getElementById('uploadAvatar');
+const imgPreview = document.querySelector('.img-preview');
+const text = document.getElementById('text');
+
+dragDropArea.addEventListener('click', () => {
+    uploadAvatar.click();
+    dragDropArea.classList.add('focus-outline');
 });
+
+uploadAvatar.addEventListener('change', updateImagePreview);
+
+function updateImagePreview() {
+    const imgFile = uploadAvatar.files[0];
+    if (!imgFile || (imgFile.size / 1024) > 5) {
+        const info = document.getElementById('info');
+        info.classList.add('error');
+        return;
+    };
+
+    imgPreview.innerHTML = '';
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(imgFile);
+    img.alt = imgFile.name;
+    imgPreview.appendChild(img);
+    imgPreview.classList.add('has-img');
+
+    if (text) text.remove();
+
+    let actionList = dragDropArea.querySelector('.action');
+    if (!actionList) {
+        actionList = document.createElement('div');
+        actionList.classList.add('action');
+
+        const removeImg = createButton('Remove Image', ['btn', 'remove-btn'], handleRemoveImage);
+        const changeImg = createButton('Change Image', ['btn', 'change-btn'], updateImagePreview, true);
+
+        actionList.appendChild(removeImg);
+        actionList.appendChild(changeImg);
+        dragDropArea.appendChild(actionList);
+    }
+}
+
+function createButton(text, classes, onClick, preventDefault = false) {
+    const button = document.createElement('button'); 
+    button.innerText = text; 
+    button.classList.add(...classes); 
+    button.addEventListener('click', (event) => {
+        if (preventDefault) {
+            event.preventDefault();
+        }
+        onClick()
+    });
+    return button;
+}
+
+function handleRemoveImage() {
+    imgPreview.innerHTML = '';
+    imgPreview.classList.remove('has-img');
+    uploadAvatar.value = '';
+    if (!dragDropArea.contains(text)) {
+        dragDropArea.appendChild(text);
+    }
+
+    const actionList = dragDropArea.querySelector('.action');
+    if (actionList) actionList.remove();
+}
